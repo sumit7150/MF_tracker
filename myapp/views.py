a665587ded70d2efa83a548601d0d632cbe30b98
Sumit Rastogi <mail.sumitrastogi@gmail.com>
2018-08-12 21:06:58 +0530

    my MF_Traker first commit

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hello(request, number):
+   text = "<h1>welcome to my app number %s!</h1>"% number
+   return HttpResponse(text)
+
